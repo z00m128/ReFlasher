cd009bee7ebb46f5fabf08cd88a986df6f40e351
z00m128 <zoom@centrum.sk>
2026-07-23 21:49:46 +0200

    SetUp and Boot updated reflecting U20260428. Separate Slim build added.

---
+++ mkreflasher-slim.sh
@@ -0,0 +1,48 @@
+sjasmplus --syntax=f --lst=reflasher.lst reflasher-slim.a80
+zmakebas -a 9998 -n ReFlasher -o 001.tap reflasher-slim.bas
+zmakebas -a 5 -n UChanger -o 003.tap uchanger.bas
+bin2tap -o 002.tap reflasher-slim.bin
+bin2tap -o firmwares.tap fw/boot.cod
+bin2tap -append -o firmwares.tap fw/Diag.cod
+bin2tap -append -o firmwares.tap fw/ESXMMC.cod
+bin2tap -append -o firmwares.tap fw/ESXIDE.cod
+bin2tap -append -o firmwares.tap fw/EasyHDD.cod
+bin2tap -append -o firmwares.tap fw/BSROM140.cod
+bin2tap -append -o firmwares.tap fw/BSDOS308.cod
+bin2tap -append -o firmwares.tap fw/MBpatch.cod
+bin2tap -append -o firmwares.tap fw/MBNMI1.cod
+bin2tap -append -o firmwares.tap fw/MBNMI2.cod
+bin2tap -append -o firmwares.tap fw/MBrsrvd.cod
+bin2tap -append -o firmwares.tap fw/DevAce.cod
+bin2tap -append -o firmwares.tap fw/slim/setup.cod
+bin2tap -append -o firmwares.tap fw/wifi.cod
+bin2tap -append -o firmwares.tap fw/H04bank0.cod
+bin2tap -append -o firmwares.tap fw/H04bank1.cod
+bin2tap -append -o firmwares.tap fw/H04bank2.cod
+bin2tap -append -o firmwares.tap fw/H04bank3.cod
+bin2tap -append -o firmwares.tap fw/H05bank0.cod
+bin2tap -append -o firmwares.tap fw/H05bank1.cod
+bin2tap -append -o firmwares.tap fw/H05bank2.cod
+bin2tap -append -o firmwares.tap fw/H05bank3.cod
+bin2tap -append -o firmwares.tap fw/64CHAR.cod
+bin2tap -append -o firmwares.tap fw/PRETTY.cod
+bin2tap -append -o firmwares.tap fw/GAMA81.cod
+bin2tap -append -o firmwares.tap fw/GROOT.cod
+bin2tap -append -o firmwares.tap fw/Derby++0.cod
+bin2tap -append -o firmwares.tap fw/Derby++1.cod
+bin2tap -append -o firmwares.tap fw/Derby++2.cod
+bin2tap -append -o firmwares.tap fw/Derby++3.cod
+bin2tap -append -o firmwares.tap fw/UserApps0.cod
+bin2tap -append -o firmwares.tap fw/UserApps1.cod
+bin2tap -append -o firmwares.tap fw/UserApps2.cod
+bin2tap -append -o firmwares.tap fw/UserApps3.cod
+bin2tap -append -o firmwares.tap fw/reserved.cod
+bin2tap -append -o firmwares.tap fw/UnoDOS.cod
+bin2tap -append -o firmwares.tap fw/CFDrv.cod
+bin2tap -append -o firmwares.tap fw/FATware.cod
+bin2tap -append -o firmwares.tap fw/MDOS3.cod
+bin2tap -append -o firmwares.tap fw/TBIOS.cod
+bin2tap -append -o firmwares.tap fw/DEMFIR.cod
+bin2tap -append -o firmwares.tap fw/+divide.cod
+cat 001.tap 002.tap firmwares.tap 003.tap > ReFlashrSlim.tap
+rm 001.tap 002.tap 003.tap firmwares.tap
